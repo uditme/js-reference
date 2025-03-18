5b8063615de76b2e1b8d2002e27bd2847f7a3976
uditme <uditmehra827@gmail.com>
2025-03-18 08:13:32 +0530

    stack and heap added

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -27,3 +27,42 @@ let state;
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+
+
+
+
+
+                                // STACK and HEAP memory
+
+
+// Stack(Primitive) [like, variables]
+// Heap(Non- Primitive) [like, arrays, objects]
+
+let out= console.log;
+
+let myname= "udit";
+let myname2= myname;
+
+myname2= "ish";
+out(myname, myname2)
+
+// copied and then changed
+
+let user1= {
+    email: "abc@gmail.com",
+    upi: "abc@sbi"
+}
+
+let user2= user1;
+
+user2.email= "udit@gmail.com";
+
+out(user1)
+out(user2)
+
+// same value in user1 and user2 as {...} is an object so it is made in heap
+// and both variables user1, and user2 are pointed to an object in heap
+
+
